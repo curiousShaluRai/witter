35ebc7d6a28b16dba528b757fc3604c5d39e7d84
Shalu Kumari <shaluk@syncroutesoftware.com>
2018-03-06 15:53:45 +0530

    Added registeration service

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -13,6 +13,14 @@ export default function IndexController(container) {
 
 IndexController.prototype._registerServiceWorker = function() {
   // TODO: register service worker
+  if(!navigator.serviceWorker) return;
+  navigator.serviceWorker.register('/sw.js').then(function(){
+    console.log("Registaration service worked");
+  }).catch(
+    function(){
+      console.log("Registration service Failed!");
+    }
+  );
 };
 
 // open a connection to the server for live updates
